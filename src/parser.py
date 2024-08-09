9c5cac1fb232fdf50fa339b3b155eca8a835c463
dagregi <dagim6066@gmail.com>
2024-08-09 11:00:30 +0300

    parser for images and links

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,6 @@
-from src.textnode import TextNode, text_type_text
+import re
+
+from src.textnode import TextNode, text_type_text, text_type_link, text_type_image
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -21,3 +23,65 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         )
 
     return new_nodes
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_nodes.append(node)
+            continue
+        original_text = node.text
+        images = extract_markdown_images(original_text)
+        if len(images) == 0:
+            new_nodes.append(node)
+            continue
+        for image in images:
+            sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown, image section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], text_type_text))
+            new_nodes.append(
+                TextNode(
+                    image[0],
+                    text_type_image,
+                    image[1],
+                )
+            )
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, text_type_text))
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_nodes.append(node)
+            continue
+        original_text = node.text
+        links = extract_markdown_links(original_text)
+        if len(links) == 0:
+            new_nodes.append(node)
+            continue
+        for link in links:
+            sections = original_text.split(f"[{link[0]}]({link[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown, link section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], text_type_text))
+            new_nodes.append(TextNode(link[0], text_type_link, link[1]))
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, text_type_text))
+    return new_nodes
+
+
+def extract_markdown_images(line):
+    return re.findall(r"!\[(.*?)\]\((.*?)\)", line)
+
+
+def extract_markdown_links(line):
+    return re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", line)
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -6,8 +6,16 @@ from src.textnode import (
     text_type_code,
     text_type_bold,
     text_type_italic,
+    text_type_image,
+    text_type_link,
 )
-from src.parser import split_nodes_delimiter
+from src.parser import (
+    split_nodes_delimiter,
+    split_nodes_image,
+    split_nodes_link,
+    extract_markdown_images,
+    extract_markdown_links,
+)
 
 
 class TestNodeDelimiter(unittest.TestCase):
@@ -53,6 +61,63 @@ class TestNodeDelimiter(unittest.TestCase):
                 TextNode("one bold", text_type_bold),
                 TextNode(" and ", text_type_text),
                 TextNode("another italic", text_type_italic),
+            ],
+            new_nodes,
+        )
+
+
+class TestImageLink(unittest.TestCase):
+    def test_image_extractor(self):
+        line = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        self.assertEqual(
+            [
+                ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+                ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"),
+            ],
+            extract_markdown_images(line),
+        )
+
+    def test_link_extractor(self):
+        line = "This is text with a [rick roll](https://i.imgur.com/aKaOqIh.gif) and [obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        self.assertEqual(
+            [
+                ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+                ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"),
+            ],
+            extract_markdown_links(line),
+        )
+
+    def test_node_links(self):
+        node = TextNode(
+            "This is text with a link ![to boot dev](https://www.boot.dev) and ![to youtube](https://www.youtube.com/@bootdotdev)",
+            text_type_text,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertEqual(
+            [
+                TextNode("This is text with a link ", text_type_text),
+                TextNode("to boot dev", text_type_image, "https://www.boot.dev"),
+                TextNode(" and ", text_type_text),
+                TextNode(
+                    "to youtube", text_type_image, "https://www.youtube.com/@bootdotdev"
+                ),
+            ],
+            new_nodes,
+        )
+    def test_node_images(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            text_type_text,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertEqual(
+            [
+                TextNode("This is text with a link ", text_type_text),
+                TextNode("to boot dev", text_type_link, "https://www.boot.dev"),
+                TextNode(" and ", text_type_text),
+                TextNode(
+                    "to youtube", text_type_link, "https://www.youtube.com/@bootdotdev"
+                ),
             ],
             new_nodes,
         )
