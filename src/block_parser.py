5e51c92edc28d5f603a156048cccca76f09dd6d1
dagregi <dagim6066@gmail.com>
2024-08-12 13:17:02 +0300

    block type identifier

--- src/block_parser.py
+++ src/block_parser.py
@@ -1,4 +1,29 @@
+import re
+
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
+
+
 def markdown_to_blocks(markdown):
     return [
         block.strip() for block in markdown.split("\n\n") if len(block.strip()) != 0
     ]
+
+
+def block_to_block_type(block):
+    if re.search(r"^(#{1,6}\s)", block):
+        return block_type_heading
+    if re.search(r"^(`{3}.*[\n\r][^]]*?`{3})", block):
+        return block_type_code
+    if re.search(r"^(>.*)", block):
+        return block_type_quote
+    if re.search(r"^\d+\.[\S\n\r]*", block):
+        return block_type_ordered_list
+    if re.search(r"^(\*|-)\s[\S\n\r]*", block):
+        return block_type_unordered_list
+    else:
+        return block_type_paragraph
--- tests/test_block_parser.py
+++ tests/test_block_parser.py
@@ -1,6 +1,15 @@
 import unittest
 
-from src.block_parser import markdown_to_blocks
+from src.block_parser import (
+    block_to_block_type,
+    markdown_to_blocks,
+    block_type_heading,
+    block_type_paragraph,
+    block_type_code,
+    block_type_quote,
+    block_type_unordered_list,
+    block_type_ordered_list,
+)
 
 
 class TestBlockParser(unittest.TestCase):
@@ -41,3 +50,37 @@ still the same paragraph.
             ],
             markdown_to_blocks(document),
         )
+
+    def test_block_to_block_type_heading(self):
+        header1 = "# Heading1"
+        header2 = "## Heading2"
+        header3 = "### Heading3"
+        header4 = "#### Heading4"
+        header5 = "##### Heading5"
+        header6 = "###### Heading6"
+        not_header = "########### Not a header"
+
+        self.assertEqual(block_type_heading, block_to_block_type(header1))
+        self.assertEqual(block_type_heading, block_to_block_type(header2))
+        self.assertEqual(block_type_heading, block_to_block_type(header3))
+        self.assertEqual(block_type_heading, block_to_block_type(header4))
+        self.assertEqual(block_type_heading, block_to_block_type(header5))
+        self.assertEqual(block_type_heading, block_to_block_type(header6))
+
+        self.assertEqual(block_type_paragraph, block_to_block_type(not_header))
+
+    def test_block_to_block_type_code(self):
+        code_block = "```\nconst greet = 'hello, world';\nconsole.log(greet)\n```"
+        self.assertEqual(block_type_code, block_to_block_type(code_block))
+
+    def test_block_to_block_type_quote(self):
+        quote = '>"What is a man?"\n>\t\tDracula'
+        self.assertEqual(block_type_quote, block_to_block_type(quote))
+
+    def test_block_to_block_type_ordered_list(self):
+        ol = "1. First\n2. \nSecond\n3. Last\nItem"
+        self.assertEqual(block_type_ordered_list, block_to_block_type(ol))
+
+    def test_block_to_block_type_unordered_list(self):
+        ul = "* First\n* \nSecond\n* Last\nItem"
+        self.assertEqual(block_type_unordered_list, block_to_block_type(ul))
